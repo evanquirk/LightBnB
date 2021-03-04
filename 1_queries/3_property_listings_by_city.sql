3120b85d16f4b59fe5861615c2f1928e7dae65cc
Evan Quirk <evan_quirk@me.com>
2021-03-03 17:57:02 -0800

    chore: add queries to table

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,11 @@
+SELECT properties.id as id,
+  title,
+  cost_per_night,
+  avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE city LIKE '%ancouver'
+  GROUP BY properties.id
+  HAVING avg(property_reviews.rating) >= 4
+  ORDER BY cost_per_night DESC
+  LIMIT 10;
